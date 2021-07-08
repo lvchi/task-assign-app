eddb24d75ebdd76c534d1d6efaa5a0262c4e6663
quanhoang288 <quanpro288@gmail.com>
2021-07-08 18:27:32 +0700

    update job crud and create view

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -19,13 +19,46 @@
 @section('button-group')
 
 	@include('components.button-group', [
+		'parentClass' => 'btn-group offset-2',
 		'buttons' => [
 			['iconClass' => 'fas fa-save', 'value' => 'Lưu', 'action' => 'save'], 
 			['iconClass' => 'fas fa-copy', 'value' => 'Lưu-sao', 'action' => 'save_copy'], 
-			['iconClass' => 'fas fa-edit', 'value' => 'Sửa', 'action' => 'edit'], 
+			['iconClass' => 'fas fa-edit', 'type' => 'button', 'value' => 'Sửa', 'action' => 'edit'], 
 			['iconClass' => 'fas fa-trash', 'value' => 'Xóa', 'action' => 'delete'], 
-			['iconClass' => 'fas fa-search', 'value' => 'Tìm kiếm', 'action' => 'search'], 
+			['iconClass' => 'fas fa-search', 'value' => 'Tìm kiếm', 'action' => 'search'],
+			['iconClass' => 'fas fa-redo', 'type' => 'button', 'value' => 'Tạo mới', 'action' => 'reset'], 
 		] 
 	])
+
+	<script>
+		$(document).ready(function() {
+
+			$('button[value="reset"]').click(function () {
+				$('.selectpicker').each(function () {
+					$(this).val('');
+					$(this).selectpicker('refresh')
+				})
+				$('input').each(function () {
+					if ($(this).attr('name') === 'status') {
+						$(this).val('Chưa nhận');
+					}
+					else {
+						$(this).val('');
+					}
+				})
+				$('#period_unit').prop('selectedIndex', -1);
+				$('textarea').val('');
+				$('#history-workplan').css('display', 'none');
+			});
+
+			$('button[value="edit"]').click(function () {
+				const jobId = $('#job_id').val();
+				if (jobId !== '') {
+					$('#note-wrapper').css('display', 'block')
+				}
+			})
+        });
+
+	</script>
 	
 @endsection
--- resources/views/layouts/job-crud.blade.php
+++ resources/views/layouts/job-crud.blade.php
@@ -1,41 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
+
     <link rel="stylesheet" href="{{ asset('css/bootstrap-select.css') }}">
     <div class="container">
+        @if (session('success'))
+            @include('components.flash-message', [
+                'modalId' => 'successModal',
+                'alertClass' => 'alert alert-sucess',
+                'message' => session('success')
+            ])
+        @elseif ($errors->has('job_id')) 
+            @include('components.flash-message', [
+                'modalId' => 'errorModal',
+                'alertClass' => 'alert alert-danger',
+                'message' => $errors->first('job_id')
+            ])
+        @endif
         <div class="row">
-            <input type="hidden" name="job_id" id="job_id">
-            <div class="col-md-9">
-               
-                <form action="{{route($routeName, $params ?? [])}}" method="{{$method}}">
-                    @csrf
 
+            
+            <div class="col-md-9">
+                <form action="{{route($routeName, $params ?? [])}}" method="{{$method}}" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" name="job_id" id="job_id" value="{{ old('job_id') }}">
                     <fieldset class="p-3 mb-3" style="border: 1px solid; border-radius: 15px">
                         <legend class="w-auto">Thông tin nghiệp vụ</legend>
                         <div class="form-group-row mb-3">
                             @include('components.searchable-input-text', [
                                 'name' => 'assigner_name',
                                 'label' => 'Người giao việc', 
-                                'listId' => 'assigner_list',
-                                'list' => $staff
+                                'options' => $staff
                             ])
-                            <input type="hidden" name="assigner_id" id="assigner_id">
+                            <i class="fas fa-asterisk" style="width: .5em; color:red"></i>
+                            <input type="hidden" name="assigner_id" id="assigner_id" value="{{ old('assigner_id') }}">
+                            @error('assigner_id')
+                                <span class="alert alert-danger ml-3 p-1 errors">{{$errors->first('assigner_id')}}</span>
+                            @enderror
                         </div>
+
                         <div class="form-group-row mb-3">
                             @include('components.searchable-input-text', [
                                 'name' => 'project_code',
                                 'label' => 'Mã dự án', 
-                                'listId' => 'project_code_list',
-                                'list' => $projects, 
+                                'options' => $projects, 
                                 'displayField' => 'code',
+                                'hiddenField' => 'name'
                             ])
 
                             @include('components.input-text', [
                                 'name' => 'project_name',
                                 'label' => '(Tên dự án)',
-                                'readonly' => true	
+                                'readonly' => true,
                             ])
-                            <input type="hidden" name="project_id" id="project_id">
+                            <input type="hidden" name="project_id" id="project_id" value="{{ old('project_id') }}">
                         </div>
 
                          
@@ -45,23 +63,22 @@
                             @include('components.searchable-input-text', [
                                 'name' => 'job_type',
                                 'label' => 'Loại công việc', 
-                                'listId' => 'job_type_list',
-                                'list' => $jobTypes, 
+                                'options' => $jobTypes, 
                             ])
-                            <input type="hidden" name="job_type_id" id="job_type_id">
+                            <input type="hidden" name="job_type_id" id="job_type_id" value="{{ old('job_type_id') }}">
                             
                         </div>
                         <div class="form-group-row mb-3">
-                            @include('components.input-text', [
+                            @include('components.input-number', [
                                 'name' => 'period',
-                                'label' => 'Kỳ'
+                                'label' => 'Kỳ',
                             ])
                             @include('components.select', [
                                 'name' => 'period_unit', 
                                 'label' => 'Đơn vị',
                                 'options' => [
                                     ['value' => 'day', 'display' => 'Ngày'],
-                                    ['value' => 'week', 'display' => 'Ngày'],
+                                    ['value' => 'week', 'display' => 'Tuần'],
                                     ['value' => 'term', 'display' => 'Kỳ'],    
                                 ]
                             ])
@@ -70,10 +87,9 @@
                             @include('components.searchable-input-text', [
                                 'name' => 'parent_job',
                                 'label' => 'Việc cha', 
-                                'listId' => 'parent_job_list',
-                                'list' => $jobs, 
+                                'options' => $jobs, 
                             ])
-                            <input type="hidden" name="parent_id" id="parent_id">
+                            <input type="hidden" name="parent_id" id="parent_id" value="{{ old('parent_id') }}">
                         </div>
                         <div class="form-group-row mb-3">
                             @include('components.input-text', [
@@ -83,27 +99,31 @@
                             @include('components.searchable-input-text', [
                                 'name' => 'priority_name',
                                 'label' => 'Độ ưu tiên', 
-                                'listId' => 'priorities',
-                                'list' => $priorities, 
+                                'options' => $priorities, 
                             ])
-                            <input type="hidden" name="priority_id" id="priority_id">
+                            <input type="hidden" name="priority_id" id="priority_id" value="{{ old('priority_id') }}">
     
                         </div>
                         <div class="form-group-row mb-3">
                             @include('components.input-text', [
                                 'name' => 'name', 
-                                'label' => 'Tên công việc'
+                                'label' => 'Tên công việc',
                             ])
+                            <i class="fas fa-asterisk" style="width: .5em; color:red"></i>
+                            @error('name')
+                               <span class="alert alert-danger ml-3 p-1 errors">{{$errors->first('name')}}</span>
+                            @enderror
                         </div>
+
                         <div class="form-group-row mb-3">
-                            @include('components.input-text', [
+                            @include('components.input-number', [
                                 'name' => 'lsx_amount', 
-                                'label' => 'Khối lượng LSX'
+                                'label' => 'Khối lượng LSX',
                             ])
                             <label>(Man day)</label>
                         </div>
                         <div class="form-group-row mb-3">
-                            @include('components.input-text', [
+                            @include('components.input-number', [
                                 'name' => 'assign_amount', 
                                 'label' => 'Khối lượng giao'
                             ])
@@ -115,7 +135,12 @@
                                 'name' => 'deadline', 
                                 'label' => 'Hạn xử lý',
                             ])
+                            <i class="fas fa-asterisk" style="width: .5em; color:red"></i>
+                            @error('deadline')
+                                <span class="alert alert-danger ml-3 p-1 errors">{{$errors->first('deadline')}}</span>
+                            @enderror
                         </div>
+
                         <div class="form-group-row mb-3">
                             @include('components.text-area', [
                                 'name' => 'description',
@@ -125,7 +150,7 @@
                         </div>
                         <div class="form-group-row mb-3">
                             @include('components.input-file', [
-                                'name' => 'files[]',
+                                'name' => 'job_files[]',
                                 'label' => 'Tệp nội dung',
                             ])
                         </div>
@@ -144,8 +169,7 @@
                                 @include('components.searchable-input-text', [
                                     'name' => 'chu-tri',
                                     'label' => 'Chủ trì', 
-                                    'listId' => 'chu-tri-list',
-                                    'list' => $staff, 
+                                    'options' => $staff, 
                                 ])
                                 <input type="hidden" name="chu-tri-id" id="chu-tri-id">
                             </div>
@@ -160,8 +184,7 @@
                                 @include('components.searchable-input-text', [
                                     'name' => 'nhan-xet',
                                     'label' => 'Theo dõi, nhận xét', 
-                                    'listId' => 'nhan-xet-list',
-                                    'list' => $staff, 
+                                    'options' => $staff, 
                                 ])
                                 <input type="hidden" name="nhan-xet-id" id="nhan-xet-id">
                             </div>
@@ -183,6 +206,13 @@
                             'value' => 'Chưa nhận'
 						])
 					</div>
+
+                    <div class="form-group-row mb-3 p-3" id="note-wrapper" style="display:  none">
+                        @include('components.text-area', [
+                            'name' => 'note',
+                            'label' => 'Ghi chú sửa đổi',
+                        ])
+					</div>
                     
                     @yield('button-group')
                 </form>
@@ -196,6 +226,10 @@
                     'min_row' => 5,
                     
                 ])
+                <div id="history-workplan" style="display: none">
+                    <a href="" class="btn btn-link p-0 mb-1 text-decoration-none">Lịch sử công việc</a>
+                    <a href="" class="btn btn-link p-0 text-decoration-none">Kế hoạch thực hiện</a>
+                </div>
             </div>
 
         </div>
@@ -205,25 +239,8 @@
 
     <script type="text/javascript">
         const handleOptionChange = (name, hiddenInputId) => {
-            document.querySelector('#' + name).addEventListener('input', function (e) {
-                let input = e.target;
-                let list = input.getAttribute('list');
-                let options = document.querySelectorAll('#' + list + ' option');
-                let hiddenInput = document.getElementById(hiddenInputId);
-                let inputValue = input.value;
-                let matched = false;
-                for(let i = 0; i < options.length; i++) {
-                    let option = options[i];
-                    if (option.innerHTML === inputValue) {
-                        hiddenInput.value = option.getAttribute('data-value');
-                        matched = true;
-                        break;
-                    }
-                }
-                
-                if (!matched) {
-                    hiddenInput.value = null;
-                }
+            $(`#${name}`).change(function (e) {
+                $(`#${hiddenInputId}`).val(e.target.value)
             });
         }
 
@@ -235,19 +252,70 @@
         } 
 
         const setValue = (selector, value) => {
-            document.querySelector(selector).value = value;
+            $(selector).val(value)
         }
 
-        datalistInputs = [
+        const setSelectedValue = (selector, value) => {
+            $(selector).selectpicker('val', value);
+            $(selector).selectpicker('refresh');
+
+        }
+
+        const initializeJobValues = (jobId) => {
+            getJob(jobId).then(job => {
+                Object.keys(job).forEach(key => {
+                    let input = document.querySelector(`#${key}`);
+                    if (input !== null) {
+                        input.value = job[key];
+                    }
+                    
+                })
+
+                setSelectedValue('#assigner_name', job.assigner_id)
+                setSelectedValue('#project_code', job.project_id);
+                setValue('#project_name', job.project ? job.project.name : '');
+
+                setSelectedValue('#job_type', job.job_type_id);
+                
+                setSelectedValue('#parent_job', job.parent_id);
+                
+                setSelectedValue('#priority_name', job.priority_id);
+            });
+        }
+
+        selectInputs = [
             {name: 'assigner_name', hiddenInputId: 'assigner_id'},
             {name: 'project_code', hiddenInputId: 'project_id'},
             {name: 'job_type', hiddenInputId: 'job_type_id'},
             {name: 'parent_job', hiddenInputId: 'parent_id'},
             {name: 'priority_name', hiddenInputId: 'priority_id'},
+            {name: 'chu-tri', hiddenInputId: 'chu-tri-id'},
+            {name: 'nhan-xet', hiddenInputId: 'nhan-xet-id'},
         ];
 
-        datalistInputs.forEach(element => {
+        selectInputs.forEach(element => {
             handleOptionChange(element.name, element.hiddenInputId);
+        });
+
+        $('#project_code').change(function () {
+            const projectName = $(this).find(':selected').attr('data-hidden');
+            $('#project_name').val(projectName);
+        });
+
+        $(document).ready(function () {
+            $("#period_unit").prop("selectedIndex", -1);
+
+            // $('#job_id').change(function () {
+            //     console.log('change');
+            //     if ($(this).val() !== '') {
+            //         const jobId = $('#job_id').val();
+            //         initializeJobValues(jobId);
+            //     }
+            // });
+            if ($('#job_id').val() !== '') {
+                const jobId = $('#job_id').val();
+                initializeJobValues(jobId);
+            }
         });
 
         document.querySelectorAll('tr').forEach(function (element) {
@@ -255,35 +323,28 @@
             if (element.id !== '') {
                 const id = element.id;                
                 element.addEventListener('click', function (e) {
-                    
-                    document.getElementById('job_id').value = id;
-                    getJob(id).then(job => {
-                        console.log(job);
-                        Object.keys(job).forEach(key => {
-                            let input = document.querySelector(`#${key}`);
-                            if (input !== null) {
-                                input.value = job[key];
-                            }
-                            
-                        })
+                    $('.alert').each(function () {
+                        $(this).alert('close')
+                    });
+                    $('#job_id').val(id);
+                    $('#history-workplan').css('display', 'block');
+                    initializeJobValues(id);
 
-                        setValue('#assigner_name', job.assigner.name);
-
-                        setValue('#project_code', job.project ? job.project.code : '');
-                        setValue('#project_name', job.project ? job.project.name : '');
-
-                        setValue('#job_type', job.type ? job.type.name : '');
-                        
-                        setValue('#parent_job', job.parent ? job.parent.name : '');
-                        
-                        setValue('#priority_name', job.priority ? job.priority.name : '');
-
-                                          
-                    })
-                })
+                });
             }
             
         });
+
+        const setCloseTimeout = (modalSelector, timeout) => {
+            $(modalSelector).modal("show").on("shown.bs.modal", function () {
+                window.setTimeout(function () {
+                    $(modalSelector).modal("hide");
+                }, timeout);
+            });
+        }
+
+        setCloseTimeout("#successModal", 5000);
+        setCloseTimeout("#errorModal", 5000);
 
         
     </script>
